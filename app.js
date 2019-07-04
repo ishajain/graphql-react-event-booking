13fc844790a5be63c0e4faf719db8881ac7dc6a1
Isha Jain <isha.jain30@gmail.com>
2019-07-04 13:53:44 +1000

    Created the first graph query schema and resolver

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import graphqlHttp from 'express-graphql'
+import { buildSchema } from 'graphql'
 const app = express()
+
 
 app.use(bodyParser.json())
 
-app.get("*",(req,res) => {
-    res.send("Hello and welcome to GraphQL React app.")
-})
+// app.get("*",(req,res) => {
+//     res.send("Hello and welcome to GraphQL React app.")
+// })
+
+app.use('/graphql',graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events : [String!]!
+        }
+        type RootMutation{
+            createEvent(name:String) : String
+        }
+        schema{
+            query:RootQuery
+            mutation:RootMutation
+        }
+    `),
+    rootValue : {
+        events : () => {
+            return ["Yoga","Cooking","Cycling"]
+        },
+        createEvent: (args) => {
+            return args.name
+        }
+    },
+    graphiql:true
+}))
 
 const PORT = process.env.PORT || 9000
 app.listen(PORT,() => {
