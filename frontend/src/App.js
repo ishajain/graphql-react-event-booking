3abd834dcb97bda0bb497d1dd8a9689a59ae19b4
Isha Jain <isha.jain30@gmail.com>
2019-07-06 00:45:21 +1000

    react navigation component is added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import { BrowserRouter,Route, Redirect, Switch} from 'react-router-dom'
-import Auth from './components/Auth'
-import Bookings from './components/Bookings'
-import Events from './components/Events'
+import Auth from './pages/Auth'
+import Bookings from './pages/Bookings'
+import Events from './pages/Events'
+import MainNavigation from './components/Navigation/MainNavigation'
+import './App.css'
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
-      <h1>Welcome to the React Application!</h1>
-      <Switch>
-        <Redirect from="/" exact to="/auth"></Redirect>
-        <Route path="/auth" component={Auth}></Route>
-        <Route path="/events" component={Events}></Route>
-        <Route path="/bookings" component={Bookings}></Route>
-      </Switch>
-      
-      </BrowserRouter>
-     
-    </div>
+        <React.Fragment>
+          <MainNavigation></MainNavigation>
+          <main className="main-content">
+          <Switch>
+            <Redirect from="/" exact to="/auth"></Redirect>
+            <Route path="/auth" component={Auth}></Route>
+            <Route path="/events" component={Events}></Route>
+            <Route path="/bookings" component={Bookings}></Route>
+          </Switch>
+          </main>
+        </React.Fragment>
+     </BrowserRouter>
+  </div>
   );
 }
 
